94f20c2b6b14e8f196dd7e26a2387ece667f7f3e
albert <albert@alberts-MacBook-Pro.local>
2021-03-04 23:35:08 +0100

    trigger

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,14 @@
 class GamesController < ApplicationController
   def index
+    ### todo
     games = Game.all
 
     json_response games
   end
 
   def create
+    Game.create(creator: current_user)
+
+    json_response :ok
   end
 end
