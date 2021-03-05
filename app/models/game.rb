40af4fac47802238e315f1fea9598296ff200a64
albert <albert@alberts-MacBook-Pro.local>
2021-03-05 22:28:01 +0100

    d

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ApplicationRecord
   belongs_to :creator, foreign_key: :creator_id, class_name: 'User'
   belongs_to :joiner, foreign_key: :joiner_id, class_name: 'User', optional: true
 
-  has_many :moves
+  has_many :moves, dependent: :destroy
 end
