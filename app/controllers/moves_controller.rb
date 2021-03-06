75863cc7fb1758d38f4d8717e0d16cf7c5af92db
albert <albert@alberts-MacBook-Pro.local>
2021-03-06 11:37:40 +0100

    check solution

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -9,6 +9,65 @@ class MovesController < ApplicationController
     move.user = current_user
     move.save
 
+    ### check if winner
+    # check(move)
+
     json_response :ok
+  end
+
+  def check(move)
+    move = Move.last
+    game = move.game
+
+    # return if game.moves.length < 5
+
+    type = move.type
+    moves = game.moves
+
+    found_winner_in_straight_lines = false
+    ['x', 'y'].each do |axis|
+      if winner_in_straight_lines(axis, moves, type)
+        found_winner_in_straight_lines = true
+        break
+      end
+    end
+
+    return true if found_winner_in_straight_lines
+
+    slope_lines_are_winners(moves, type)
+  end
+
+  def slope_lines_are_winners(moves, type)
+    slopes = [
+      [{pos_x: -1, pos_y: -1}, {pos_x: 0, pos_y: 0}, {pos_x: 1, pos_y: 1}],
+      [{pos_x: -1, pos_y: +1}, {pos_x: 0, pos_y: 0}, {pos_x: 1, pos_y: -1}]
+    ]
+
+    is_winner_found = false
+    slopes.each do |points|
+      matching_moves = points.map do |point| 
+        moves.where(pos_x: point[:pos_x], pos_y: point[:pos_y]).first
+      end
+      
+      matching_moves = matching_moves.compact
+      next if matching_moves.length < 3
+
+      if matching_moves.all? { |move| move.type == type }
+        is_winner_found = true
+        break
+      end
+    end
+
+    is_winner_found
+  end
+
+  def winner_in_straight_lines(axis, moves, type)
+    options = [-1, 0, 1]
+    results = options.map do |option|
+      matching_moves = moves.where("pos_#{axis}": option)
+      matching_moves.all? { |move| move.type == type } && matching_moves.length === 3
+    end 
+
+    results.any? { |result| result }
   end
 end
--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,13 @@
 class Move < ApplicationRecord
   belongs_to :game
   belongs_to :user
+
+  def type
+    creator = self.game.creator
+    if creator == User.first
+      return 'cross'
+    else 
+      return 'noutgh'
+    end
+  end
 end
